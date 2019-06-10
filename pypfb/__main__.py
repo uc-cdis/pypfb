66a5dedcfd5c74187f797c19cdfbfb956ed47c79
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2019-06-10 11:10:18 -0500

    chore(help): update help strings

--- pypfb/__main__.py
+++ pypfb/__main__.py
@@ -26,19 +26,19 @@ def main():
 
     subparsers = parser.add_subparsers(dest='cmd')
 
-    read_cmd = subparsers.add_parser('show', help='')
+    read_cmd = subparsers.add_parser('show', help='Show schema or records of the PFB file')
     read_cmd.add_argument('input', type=str, help='Path to PFB file')
     read_cmd.add_argument('-s', '--schema', action='store_true', help='Show PFB file schema')
     read_cmd.add_argument('--limit', type=int, required=False,
                           help='How many entries to show, -1 for all; ignored for "schema"')
 
-    dict2pfb_cmd = subparsers.add_parser('dict2pfb', help='')
+    dict2pfb_cmd = subparsers.add_parser('dict2pfb', help='Convert datadictionary into PFB file with schema')
     dict2pfb_cmd.add_argument('-d', '--dictionary', required=True, type=str, help='Link to dictionary URL')
     dict2pfb_cmd.add_argument('-o', '--output', required=True, type=argparse.FileType('wb'), help='Output PFB file')
 
-    json2pfb_cmd = subparsers.add_parser('json2pfb', help='')
+    json2pfb_cmd = subparsers.add_parser('json2pfb', help='Convert JSON files correspond to datadictionary into PFB file')
     json2pfb_cmd.add_argument('dir', type=str, help='Path to directory with input JSON files')
-    json2pfb_cmd.add_argument('-s', '--schema', type=str, required=True, help='Filename for raw PFB file')
+    json2pfb_cmd.add_argument('-s', '--schema', type=str, required=True, help='Filename for schema PFB file')
     json2pfb_cmd.add_argument('-o', '--output', type=str, required=True, help='Filename for resulting PFB file')
     json2pfb_cmd.add_argument('--program', type=str, required=True, help='Name of the program')
     json2pfb_cmd.add_argument('--project', type=str, required=True, help='Name of the project')
@@ -46,19 +46,19 @@ def main():
     rename = subparsers.add_parser('rename', help='')
 
     sub_rename = rename.add_subparsers(dest='rename')
-    node_rename = sub_rename.add_parser('node')
+    node_rename = sub_rename.add_parser('node', help="Rename node")
     node_rename.add_argument('-i', '--input', type=str, required=True, help='')
     node_rename.add_argument('-o', '--output', type=str, required=True, help='')
     node_rename.add_argument('--name_from', type=str, required=True, help='')
     node_rename.add_argument('--name_to', type=str, required=True, help='')
 
-    type_rename = sub_rename.add_parser('type')
+    type_rename = sub_rename.add_parser('type', help="Rename type (not implemented)")
     type_rename.add_argument('-i', '--input', type=str, required=True, help='')
     type_rename.add_argument('-o', '--output', type=str, required=True, help='')
     type_rename.add_argument('--name_from', type=str, required=True, help='')
     type_rename.add_argument('--name_to', type=str, required=True, help='')
 
-    enum_rename = sub_rename.add_parser('type')
+    enum_rename = sub_rename.add_parser('enum', help="Rename enum (not implemented)")
     enum_rename.add_argument('-i', '--input', type=str, required=True, help='')
     enum_rename.add_argument('-o', '--output', type=str, required=True, help='')
     enum_rename.add_argument('--name_from', type=str, required=True, help='')
@@ -86,6 +86,9 @@ def main():
 
     elif args.cmd == 'json2pfb':
         PFBFile.from_json(args.schema, args.dir, args.output, args.program, args.project)
+
+    elif args.cmd == 'make':
+
 
     elif args.cmd == 'rename':
         if args.rename == 'node':
