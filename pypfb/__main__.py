1a84f87dea352fbbac550fed0888e3631feb3cd2
Fantix King <fantix@uchicago.edu>
2019-07-02 13:17:11 -0500

    use Click

--- pypfb/__main__.py
+++ pypfb/__main__.py
@@ -1,8 +1,9 @@
-import os
-import argparse
 import itertools
+import json
 import logging.config
+import os
 
+import click
 import yaml
 
 from .avro_utils.avro_schema import AvroSchema
@@ -26,141 +27,137 @@ else:
 log = logging.getLogger()
 
 
+@click.group()
 def main():
-    parser = argparse.ArgumentParser(description="PFB tool")
+    """PFB: Portable Format for Biomedical Data."""
 
-    subparsers = parser.add_subparsers(dest="cmd")
 
-    read_cmd = subparsers.add_parser(
-        "show", help="Show schema or records of the PFB file"
-    )
-    read_cmd.add_argument("input", type=str, help="Path to PFB file")
-    read_cmd.add_argument(
-        "-s", "--schema", action="store_true", help="Show PFB file schema"
-    )
-    read_cmd.add_argument(
-        "--limit",
-        type=int,
-        required=False,
-        help='How many entries to show, -1 for all; ignored for "schema"',
-    )
+@main.command()
+@click.argument("input_file", metavar="PFB")
+@click.option("-s", "--schema", is_flag=True, help="Show PFB file schema.")
+@click.option(
+    "--limit",
+    type=int,
+    help='How many entries to show, -1 for all; ignored for "schema".',
+)
+def show(input_file, schema, limit):
+    """Show schema or records of the PFB file."""
+    if schema:
+        print(json.dumps(Gen3PFB(input_file).read_metadata()))
+    else:
+        limit = limit if limit != -1 else None
+        for r in itertools.islice(Gen3PFB(input_file).read_records(), limit):
+            print(json.dumps(r))
 
-    if init_dictionary is not None:
-        dict2pfb_cmd = subparsers.add_parser(
-            "dict2pfb", help="Convert datadictionary into PFB file with schema"
-        )
-        dict2pfb_cmd.add_argument(
-            "-d", "--dictionary", required=True, type=str, help="Link to dictionary URL"
-        )
-        dict2pfb_cmd.add_argument(
-            "-o",
-            "--output",
-            required=True,
-            type=argparse.FileType("wb"),
-            help="Output PFB file",
-        )
 
-    json2pfb_cmd = subparsers.add_parser(
-        "json2pfb", help="Convert JSON files correspond to datadictionary into PFB file"
-    )
-    json2pfb_cmd.add_argument(
-        "dir", type=str, help="Path to directory with input JSON files"
-    )
-    json2pfb_cmd.add_argument(
-        "-s", "--schema", type=str, required=True, help="Filename for schema PFB file"
-    )
-    json2pfb_cmd.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        required=True,
-        help="Filename for resulting PFB file",
-    )
-    json2pfb_cmd.add_argument(
-        "--program", type=str, required=True, help="Name of the program"
-    )
-    json2pfb_cmd.add_argument(
-        "--project", type=str, required=True, help="Name of the project"
-    )
+@main.group("from")
+def from_command():
+    """Generate PFB from other data formats."""
 
-    make_cmd = subparsers.add_parser("make", help="Make blank record")
-    make_cmd.add_argument("input", type=str, help="Path to PFB file")
-    make_cmd.add_argument("-n", "--node", type=str, help="Node to create")
 
-    add_cmd = subparsers.add_parser(
-        "add", description="Add a record to PFB file from a minified JSON file"
+if init_dictionary is not None:
+
+    @from_command.command("dict")
+    @click.argument("url")
+    @click.option(
+        "-o", "--output", required=True, type=click.File("wb"), help="Output PFB file."
     )
-    add_cmd.add_argument(
-        "PFB_file",
-        type=str,
-        default="test.pfb",
-        help="pfb file to add record to. Default = test.pfb",
-    )
-    add_cmd.add_argument(
-        "JSON_file",
-        type=str,
-        default="test.json",
-        help="JSON file to add into the pfb file. Default = test.json",
-    )
-
-    rename = subparsers.add_parser("rename", help="Rename different parts of schema")
-
-    sub_rename = rename.add_subparsers(dest="rename")
-    node_rename = sub_rename.add_parser("node", help="Rename node")
-    node_rename.add_argument("-i", "--input", type=str, required=True, help="")
-    node_rename.add_argument("-o", "--output", type=str, required=True, help="")
-    node_rename.add_argument("--name_from", type=str, required=True, help="")
-    node_rename.add_argument("--name_to", type=str, required=True, help="")
-
-    type_rename = sub_rename.add_parser("type", help="Rename type (not implemented)")
-    type_rename.add_argument("-i", "--input", type=str, required=True, help="")
-    type_rename.add_argument("-o", "--output", type=str, required=True, help="")
-    type_rename.add_argument("--name_from", type=str, required=True, help="")
-    type_rename.add_argument("--name_to", type=str, required=True, help="")
-
-    enum_rename = sub_rename.add_parser("enum", help="Rename enum (not implemented)")
-    enum_rename.add_argument("-i", "--input", type=str, required=True, help="")
-    enum_rename.add_argument("-o", "--output", type=str, required=True, help="")
-    enum_rename.add_argument("--field_name", type=str, required=True, help="")
-    enum_rename.add_argument("--val_from", type=str, required=True, help="")
-    enum_rename.add_argument("--val_to", type=str, required=True, help="")
-
-    args = parser.parse_args()
-
-    if args.cmd == "dict2pfb":
-        dictionary, _ = init_dictionary(args.dictionary)
+    def dict_command(url, output):
+        """Convert Gen3 data dictionary at URL into PFB file."""
+        dictionary, _ = init_dictionary(url)
         schema = dictionary.schema
 
-        log.info("Using dictionary: {}".format(args.dictionary))
+        log.info("Using dictionary: {}".format(url))
 
         avro_schema = AvroSchema.from_dictionary(schema)
-        avro_schema.write(args.output)
+        avro_schema.write(output)
 
-    elif args.cmd == "json2pfb":
-        Gen3PFB.from_json(
-            args.schema, args.dir, args.output, args.program, args.project
-        )
 
-    elif args.cmd == "show":
-        if args.schema:
-            print(Gen3PFB(args.input).read_metadata())
-        else:
-            limit = args.limit if args.limit != -1 else None
-            for r in itertools.islice(Gen3PFB(args.input).read_records(), limit):
-                print(r)
-    elif args.cmd == "make":
-        Gen3PFB(args.input).make_record(args.node)
+@from_command.command("json")
+@click.argument("path")
+@click.option(
+    "-s",
+    "--schema",
+    metavar="FILENAME",
+    required=True,
+    help="The PFB file to load the schema from.",
+)
+@click.option(
+    "-o", "--output", metavar="FILENAME", required=True, help="The result PFB file."
+)
+@click.option("--program", required=True, help="Name of the program.")
+@click.option("--project", required=True, help="Name of the project.")
+def json_command(path, schema, output, program, project):
+    """Convert JSON files under PATH into a PFB file."""
+    Gen3PFB.from_json(schema, path, output, program, project)
 
-    elif args.cmd == "add":
-        Gen3PFB(args.PFB_file).add_record(args.JSON_file)
 
-    elif args.cmd == "rename":
-        if args.rename == "node":
-            Gen3PFB(args.input).rename_node(args.output, args.name_from, args.name_to)
-        elif args.rename == "enum":
-            Gen3PFB(args.input).rename_field_enum(
-                args.output, args.field_name, args.val_from, args.val_to
-            )
+@main.command()
+@click.argument("path", metavar="PFB")
+@click.option("-n", "--node", required=True, help="Node to create.")
+def make(path, node):
+    """Make blank record from the PFB file."""
+    Gen3PFB(path).make_record(node)
+
+
+@main.command()
+@click.argument("json_file", metavar="JSON")
+@click.argument("pfb_file", metavar="PFB")
+def add(json_file, pfb_file):
+    """Add a record from a minified JSON file to the PFB file."""
+    Gen3PFB(pfb_file).add_record(json_file)
+
+
+@main.group()
+@click.option(
+    "-i",
+    "--input",
+    "input_file",
+    metavar="FILENAME",
+    required=True,
+    help="Source PFB file.",
+)
+@click.option(
+    "-o",
+    "--output",
+    "output_file",
+    metavar="FILENAME",
+    required=True,
+    help="Destination PFB file.",
+)
+@click.pass_context
+def rename(ctx, input_file, output_file):
+    """Rename different parts of schema."""
+    ctx.ensure_object(dict)
+    ctx.obj["pfb"] = Gen3PFB(input_file)
+    ctx.obj["output"] = output_file
+
+
+@rename.command("node")
+@click.option("--from", "name_from", required=True, help="")
+@click.option("--to", "name_to", required=True, help="")
+@click.pass_context
+def node_command(ctx, name_from, name_to):
+    """Rename node."""
+    ctx.obj["pfb"].rename_node(ctx.obj["output"], name_from, name_to)
+
+
+@rename.command("type")
+@click.option("--from", "name_from", required=True, help="")
+@click.option("--to", "name_to", required=True, help="")
+@click.pass_context
+def type_command(ctx, name_from, name_to):
+    """Rename type (not implemented)."""
+
+
+@rename.command("enum")
+@click.option("--field", required=True, help="")
+@click.option("--from", "val_from", required=True, help="")
+@click.option("--to", "val_to", required=True, help="")
+@click.pass_context
+def enum_command(ctx, field, val_from, val_to):
+    """Rename enum."""
+    ctx.obj["pfb"].rename_field_enum(ctx.obj["output"], field, val_from, val_to)
 
 
 if __name__ == "__main__":
