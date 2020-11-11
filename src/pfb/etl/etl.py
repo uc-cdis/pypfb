8e40ce93ceb2e6cdb7b16b56f361edd544bae2fb
Giang Bui <giangbui0816@gmail.com>
2020-11-10 23:44:41 -0600

    feat(parallel): make the implementation parrallelable

--- src/pfb/etl/etl.py
+++ src/pfb/etl/etl.py
@@ -8,10 +8,14 @@ class ETLHelper:
 
     def __init__(self, base_url, access_token):
         self.base_url = base_url
-        self.headers = {"Content-Type": "application/json"}
+        self.token = access_token
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
 
     async def insert_document(self, index, document, id):
-        """Asynchronous update authz field for did"""
+        """Asynchronous insert document to elasticsearch"""
         url = f"{self.base_url}/{index}/_doc/{id}"
         async with ClientSession() as session:
             async with session.put(url, json=document, headers=self.headers,) as r:
@@ -70,10 +74,41 @@ class ETL:
                 self.root_node_ids.append(relation["dst_id"])
             self.links[relation["dst_id"]].append(submitter_id)
 
+    def find_all_node_ids(self, root_id):
+        """
+        Find all node ids that can be reached from root id
+
+        Args:
+            root_id(str): root submitter_id
+
+        Returns:
+            result(dict): group all submitter_id by the node name as the key
+        """
+        queue = [root_id]
+        visited = {}
+        idx = 0
+        while idx < len(queue):
+            top = queue[idx]
+            visited[top] = 1
+            for node_id in self.links.get(top, []):
+                if node_id not in visited:
+                    visited[node_id] = 1
+                    queue.append(node_id)
+            idx += 1
+
+        submitted_ids = visited.keys()
+        result = {}
+        for submitted_id in submitted_ids:
+            node_name = self.find_node_name(submitted_id)
+            if node_name not in result:
+                result[node_name] = set()
+            result[node_name].add(submitted_id)
+        return result
+
     def build_spanning_table(self, root_id):
         def dfs(root_id, chosen_node_ids):
             """
-            Spanning from the root with the current node values
+            Spanning from the root with the current node ids or submitter id
             The result is the list of the nodes that can reach from the root
             """
             stack = [root_id]
@@ -89,7 +124,17 @@ class ETL:
 
         def pick_k_th_node(k, node_name_list, node_ids, chosen_node_ids):
             """
-            Pick the k-th candidate from node values
+            Pick the k-th candidate from node ids and find the largest connected component
+            that contains the root node
+
+            Args:
+                k(int): the control value
+                node_name_list(list): the list of node names that have paths to the root node
+                node_ids(list): the list of submitter_id that have paths to the root_id
+                chose_node_ids(list): keep track the current chosen node ids
+
+            Return:
+                None
             """
             if k == len(node_name_list):
                 solution = dfs(root_id, set(chosen_node_ids))
@@ -111,22 +156,11 @@ class ETL:
                     k + 1, node_name_list, node_ids, chosen_node_ids + [node_value],
                 )
 
-        node_name_list = set()
-        node_ids = {}
-        for k, v in self.links.items():
-            for e in [k] + v:
-                node_name = e.split("_")[0]
-                for name in self.schema_node_names:
-                    if name in e:
-                        node_name = name
-                        break
-                node_name_list.add(node_name)
-                if node_name not in node_ids:
-                    node_ids[node_name] = set()
-                node_ids[node_name].add(e)
-        node_name_list = list(node_name_list)
         chosen_node_ids = []
-        pick_k_th_node(0, node_name_list, node_ids, chosen_node_ids)
+        node_name_list = [node_name for node_name in self.find_all_node_ids(root_id)]
+        pick_k_th_node(
+            0, node_name_list, self.find_all_node_ids(root_id), chosen_node_ids
+        )
 
     async def submit_data(self):
         for node_name, values in self.node_rows.items():
@@ -135,7 +169,7 @@ class ETL:
                     node_name, value, value["submitter_id"]
                 )
 
-        # TODO: implement batch insert
+        # TODO: implement batch insertion
         i = 0
         for row in self.spanning_tree_rows:
             await self.helper.insert_document(
@@ -155,7 +189,10 @@ class ETL:
 
 
 if __name__ == "__main__":
-    etl = ETL("http://localhost:9200", "", "tests/pfb-data/test.avro", "participant")
+    etl = ETL(
+        "http://localhost:9200", "", "tests/pfb-data/gtexdictionary_data.avro", "case"
+    )
     etl.transform()
-    etl.build_spanning_table("participant_metalinguistics_monofilm")
+    for root_id in etl.root_node_ids:
+        etl.build_spanning_table(root_id)
     asyncio.run(etl.submit_data())
