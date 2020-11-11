dd0d5927c56161ad475e1b4fe127789e13854922
Giang Bui <giangbui0816@gmail.com>
2020-11-11 17:27:55 -0600

    feat(node_name): make node_id pair with node name

--- src/pfb/etl/etl.py
+++ src/pfb/etl/etl.py
@@ -29,12 +29,12 @@ class ETL:
         self.pfbfile = pfbfile
         self.root_name = root_name
 
-        self.schema_node_names = []
+        # self.schema_node_names = []
         self.links = {}
         self.root_node_ids = []
         self.spanning_tree_rows = []
         self.node_rows = {}
-        self.schema = None
+        # self.schema = None
 
         self.helper = ETLHelper(base_url, access_token)
 
@@ -44,14 +44,14 @@ class ETL:
     def transform(self):
         with open(self.pfbfile, "rb") as fo:
             avro_reader = reader(fo)
-            # read schema
-            self.schema = avro_reader.next()
-            self.schema_node_names = []
-            for node in self.schema["object"]["nodes"]:
-                self.schema_node_names.append(node["name"])
-            self.schema_node_names = sorted(
-                self.schema_node_names, key=lambda x: len(x), reverse=True
-            )
+            # skip schema
+            avro_reader.next()
+
+            # for node in self.schema["object"]["nodes"]:
+            #     self.schema_node_names.append(node["name"])
+            # self.schema_node_names = sorted(
+            #     self.schema_node_names, key=lambda x: len(x), reverse=True
+            # )
 
             # iterate each record
             for record in avro_reader:
@@ -59,22 +59,28 @@ class ETL:
 
     def _process(self, record):
         data = record["object"]
+        if "submitter_id" not in data:
+            # skip the data that does not have submitter_id
+            return
         if record["name"] not in self.node_rows:
             self.node_rows[record["name"]] = []
         self.node_rows[record["name"]].append(data)
         relations = record["relations"]
         submitter_id = data["submitter_id"]
         for relation in relations:
-            if relation["dst_id"] not in self.links:
-                self.links[relation["dst_id"]] = []
             if (
-                self.root_name in relation["dst_id"]
+                self.root_name == relation["dst_name"]
                 and relation["dst_id"] not in self.root_node_ids
             ):
                 self.root_node_ids.append(relation["dst_id"])
-            self.links[relation["dst_id"]].append(submitter_id)
 
-    def find_all_node_ids(self, root_id):
+            if (relation["dst_id"], relation["dst_name"]) not in self.links:
+                self.links[(relation["dst_id"], relation["dst_name"])] = []
+            self.links[(relation["dst_id"], relation["dst_name"])].append(
+                (submitter_id, record["name"])
+            )
+
+    def find_all_node_ids(self, root):
         """
         Find all node ids that can be reached from root id
 
@@ -84,36 +90,35 @@ class ETL:
         Returns:
             result(dict): group all submitter_id by the node name as the key
         """
-        queue = [root_id]
+        queue = [root]
         visited = {}
         idx = 0
         while idx < len(queue):
-            top = queue[idx]
-            visited[top] = 1
-            for node_id in self.links.get(top, []):
-                if node_id not in visited:
-                    visited[node_id] = 1
-                    queue.append(node_id)
+            submitter_id, node_name = queue[idx]
+
+            unique_key = (submitter_id, node_name)
+            visited[unique_key] = 1
+            for node_id, node_name in self.links.get(unique_key, []):
+                if (node_id, node_name) not in visited:
+                    queue.append((node_id, node_name))
             idx += 1
 
-        submitted_ids = visited.keys()
         result = {}
-        for submitted_id in submitted_ids:
-            node_name = self.find_node_name(submitted_id)
+        for submitted_id, node_name in visited.keys():
             if node_name not in result:
                 result[node_name] = set()
             result[node_name].add(submitted_id)
         return result
 
-    def build_spanning_table(self, root_id):
-        def dfs(root_id, chosen_node_ids):
+    def build_spanning_table(self, root):
+        def dfs(root, chosen_node_ids):
             """
             Spanning from the root with the current node ids or submitter id
             The result is the list of the nodes that can reach from the root
             """
-            stack = [root_id]
+            stack = [root]
             visited = set()
-            visited.add(root_id)
+            visited.add(root)
             while stack:
                 top = stack.pop()
                 for v in self.links.get(top, []):
@@ -137,7 +142,7 @@ class ETL:
                 None
             """
             if k == len(node_name_list):
-                solution = dfs(root_id, set(chosen_node_ids))
+                solution = dfs(root, set(chosen_node_ids))
                 delete_list = []
                 for r in self.spanning_tree_rows:
                     # ignore the spanning tree whose parent already exists
@@ -153,14 +158,18 @@ class ETL:
 
             for node_value in node_ids[node_name_list[k]]:
                 pick_k_th_node(
-                    k + 1, node_name_list, node_ids, chosen_node_ids + [node_value],
+                    k + 1,
+                    node_name_list,
+                    node_ids,
+                    chosen_node_ids + [(node_value, node_name_list[k])],
                 )
 
         chosen_node_ids = []
-        node_name_list = [node_name for node_name in self.find_all_node_ids(root_id)]
-        pick_k_th_node(
-            0, node_name_list, self.find_all_node_ids(root_id), chosen_node_ids
-        )
+
+        # Find all related node ids that have a path to the root_id
+        related_node_ids = self.find_all_node_ids(root)
+        node_name_list = [node_name for node_name in related_node_ids]
+        pick_k_th_node(0, node_name_list, related_node_ids, chosen_node_ids)
 
     async def submit_data(self):
         for node_name, values in self.node_rows.items():
@@ -172,27 +181,21 @@ class ETL:
         # TODO: implement batch insertion
         i = 0
         for row in self.spanning_tree_rows:
-            await self.helper.insert_document(
-                "spanning_tree_index",
-                dict(zip(list(map(self.find_node_name, row)), row)),
-                i,
-            )
+            submission_json = {}
+            for (node_id, node_name) in row:
+                submission_json[node_name] = node_id
+            await self.helper.insert_document("spanning_tree_index", submission_json, i)
             i += 1
-
-    def find_node_name(self, submitter_id):
-        for node_name in self.schema_node_names:
-            if node_name in submitter_id:
-                return node_name
-        raise Exception(
-            f"ERROR: the avro file is in wrong format. There is no node for {submitter_id}"
-        )
 
 
 if __name__ == "__main__":
     etl = ETL(
-        "http://localhost:9200", "", "tests/pfb-data/gtexdictionary_data.avro", "case"
+        "http://localhost:9200", "", "./tests/pfb-data/gtexdictionary_data.avro", "case"
     )
     etl.transform()
+    n = 0
     for root_id in etl.root_node_ids:
-        etl.build_spanning_table(root_id)
+        print(f"{n}/{len(etl.root_node_ids)}")
+        n = n + 1
+        etl.build_spanning_table((root_id, etl.root_name))
     asyncio.run(etl.submit_data())
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -4,23 +4,19 @@ from pfb.etl.etl import ETL
 def test_etl():
     etl = ETL("http://localhost:9200", "", "tests/pfb-data/test.avro", "participant")
     etl.links = {
-        "A_1": ["B_1", "C_1", "C_2", "B_2"],
-        "B_1": ["D_1"],
-        "D_1": ["C_1"],
-        "C_1": ["D_2", "B_3"],
-        "B_2": [],
-        "C_2": [],
-        "D_2": [],
-        "B_3": [],
+        ("A_1", "A"): [("B_1", "B"), ("C_1", "C"), ("C_2", "C"), ("B_2", "B")],
+        ("B_1", "B"): [("D_1", "D")],
+        ("D_1", "D"): [("C_1", "C")],
+        ("C_1", "C"): [("D_2", "D"), ("B_3", "B")],
     }
-    etl.build_spanning_table("A_1")
+    etl.build_spanning_table(("A_1", "A"))
     assert len(etl.spanning_tree_rows) == 6
     for r in etl.spanning_tree_rows:
         assert r in [
-            {"C_2", "B_2", "A_1"},
-            {"C_2", "B_1", "D_1", "A_1"},
-            {"C_1", "B_2", "D_2", "A_1"},
-            {"C_1", "B_1", "D_2", "A_1"},
-            {"C_1", "B_1", "D_1", "A_1"},
-            {"C_1", "B_3", "D_2", "A_1"},
+            {("C_2", "C"), ("B_2", "B"), ("A_1", "A")},
+            {("C_2", "C"), ("B_1", "B"), ("D_1", "D"), ("A_1", "A")},
+            {("C_1", "C"), ("B_2", "B"), ("D_2", "D"), ("A_1", "A")},
+            {("C_1", "C"), ("B_1", "B"), ("D_2", "D"), ("A_1", "A")},
+            {("C_1", "C"), ("B_1", "B"), ("D_1", "D"), ("A_1", "A")},
+            {("C_1", "C"), ("B_3", "B"), ("D_2", "D"), ("A_1", "A")},
         ]
