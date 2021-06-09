01b826b9343583685d487e95de1915317a522e5a
Michael Lukowski <lukowski9@gmail.com>
2021-06-09 11:33:09 -0500

    paramaterizing tests

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -4,6 +4,8 @@ import json
 import os
 import shutil
 
+import pytest
+
 from fastavro import reader
 
 from pfb.base import decode_enum, encode_enum, str_hook
@@ -21,337 +23,37 @@ def _test_schema(r):
                                 decode_enum(symbol)
 
 
-def test_from_dict_niaid(runner, invoke):
+my_dictionaries = [
+    "https://s3.amazonaws.com/dictionary-artifacts/ndhdictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/bhcdictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/gtexdictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/genomel-dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/kf-dictionary/develop/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/acctdictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/charlie-datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/canine_dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/covid19-datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/midrc_dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/flu-datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/heal_dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/ibdgc-dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/tcga_dictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/jcoin_datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/microbiome_datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/tb-datadictionary/master/schema.json",
+    "https://s3.amazonaws.com/dictionary-artifacts/vadictionary/master/schema.json",
+]
+
+
+@pytest.mark.parametrize("dictionary", my_dictionaries)
+def test_dictionary(runner, invoke, dictionary):
     with runner.isolated_filesystem():
         result = invoke(
             "from",
             "-o",
             "test.avro",
             "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/ndhdictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_bhc(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/bhcdictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_gtex(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/gtexdictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_genomel(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/genomel-dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_kf(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/kf-dictionary/develop/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_acct(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/acctdictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_charlie(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/charlie-datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_canine(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/canine_dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_covid19(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/covid19-datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_bhc(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/bhcdictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_bpa(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/bpadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_midrc(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/midrc_dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_flu(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/flu-datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_heal(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/heal_dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_ibdgc(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/ibdgc-dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_tcga(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/tcga_dictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_jcoin(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/jcoin_datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_microbiome(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/microbiome_datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_tb(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/tb-datadictionary/master/schema.json",
-        )
-        assert result.exit_code == 0, result.output
-
-        with open("test.avro", "rb") as f:
-            r = reader(f)
-            _test_schema(r)
-            assert len(list(r)) == 1
-
-
-def test_from_dict_va(runner, invoke):
-    with runner.isolated_filesystem():
-        result = invoke(
-            "from",
-            "-o",
-            "test.avro",
-            "dict",
-            "https://s3.amazonaws.com/dictionary-artifacts/vadictionary/master/schema.json",
+            dictionary,
         )
         assert result.exit_code == 0, result.output
 
