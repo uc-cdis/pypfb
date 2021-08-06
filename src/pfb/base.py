d00108a5712f91769f9acbdefc29d3822262d936
Michael Lukowski <lukowski9@gmail.com>
2021-08-06 09:11:41 -0500

    black code

--- src/pfb/base.py
+++ src/pfb/base.py
@@ -48,7 +48,6 @@ def unicode_decode(matchobj):
     return unicodeRep
 
 
-
 def encode_enum(enumValue):
     encodedValue = re.sub(
         "^[0-9]|[^A-Za-z0-9]", unicode_encode, str(enumValue), flags=re.UNICODE
